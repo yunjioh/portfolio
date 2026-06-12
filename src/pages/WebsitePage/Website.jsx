433fc68edc8209966c46f4fff4f5996077638cf9
yunjioh <115560056+yunjioh@users.noreply.github.com>
2026-06-12 22:56:28 +0900

    Update Website.jsx

--- src/pages/WebsitePage/Website.jsx
+++ src/pages/WebsitePage/Website.jsx
@@ -3,7 +3,6 @@ import "./Website.css"
 import Title from "../../components/Title";
 import SubTitle from "../../components/SubTitle";
 import ScrollReveal from "../../components/ScrollReveal";
-import Badge from "../../components/Badge";
 import { Link } from "react-router-dom";
 
 export const websiteProjects = [
